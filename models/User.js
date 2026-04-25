a0925b1805dbc42b0743fa3e5389d20c61751f65
rahul seq <rahul@sequencesurface.com>
2026-04-25 13:12:56 +0530

    ccc

--- models/User.js
+++ models/User.js
@@ -80,20 +80,7 @@ const userSchema = mongoose.Schema({
 }, { timestamps: true });
 
 
-router.post('/google-login', async (req, res) => {
-    const { email, name, googleId, picture } = req.body;
-    try {
-        let user = await User.findOne({ email });
-        if (!user) {
-            // Auto-register if first time
-            user = await User.create({ name, email, googleId, picture, password: '' });
-        }
-        const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET, { expiresIn: '7d' });
-        res.json({ token, user });
-    } catch (err) {
-        res.status(500).json({ message: 'Google login failed' });
-    }
-});
+
 
 
 
